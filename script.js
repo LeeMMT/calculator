04e90a47dd966b8c06b0f9296012153d26e758e1
Marcus <marcuslt95@gmail.com>
2021-05-09 01:34:14 +0100

    adding event listeners

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+const display = document.querySelector('#display p');
+const numBtn = document.querySelectorAll('.num-symbol');
+let displayText = '';
+
+//operator functions
+
 const add = function(num1, num2) {
     return +num1 + +num2;
-}
+};
 
 const subtract = function(num1, num2) {
     return +num1 - +num2;
-}
+};
 
 const multiply = function(num1, num2) {
     return +num1 * +num2;
-}
+};
 
 const divide = function(num1, num2) {
     return +num1 / +num2;
-}
+};
 
 const operate = function(num1, op, num2) {
     switch (op) {
@@ -28,4 +34,12 @@ const operate = function(num1, op, num2) {
             divide(num1, num2);
             break;
     }
+};
+
+//Display
+
+const enterDigit = function() {
+    alert(numBtn.textContent);
 }
+
+numBtn.forEach(element => addEventListener('click', enterDigit));
