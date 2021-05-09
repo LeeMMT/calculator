8e3074389c438d92cdb6671365bb46f55591903b
Marcus <marcuslt95@gmail.com>
2021-05-09 19:24:23 +0100

    Basic functionality in place

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const display = document.querySelector('#display p');
 const numBtn = document.querySelectorAll('.num-symbol');
 const operatorBtn = document.querySelectorAll('.operator-symbol');
-const clearBtn = document.querySelector('#clear');
 const equalsBtn = document.querySelector('#equals');
+const backSpaceBtn = document.querySelector('#backspace');
+const clearBtn = document.querySelector('#clear');
 let num1 = '';
 let operator = '';
 let num2 = '';
@@ -28,17 +29,29 @@ const divide = function(num1, num2) {
 const operate = function() {
     if (num1 && num2) {
     switch(operator) {
-        case '+':
-            console.log('hi');
+        case '＋':
+            display.textContent = add(num1, num2);
+            num1 = add(num1, num2);
+            num2 = '';
+            operator = '';
             break;
-        case '-':
-            subtract(num1, num2);
+        case '－':
+            display.textContent = subtract(num1, num2);
+            num1 = subtract(num1, num2);
+            num2 = '';
+            operator = '';
             break;
-        case '*':
-            multiply(num1, num2);
+        case '×':
+            display.textContent = multiply(num1, num2);
+            num1 = multiply(num1, num2);
+            num2 = '';
+            operator = '';
             break;
-        case '/':
-            divide(num1, num2);
+        case '÷':
+            display.textContent = divide(num1, num2);
+            num1 = divide(num1, num2);
+            num2 = '';
+            operator = '';
             break;
     }
     }
@@ -48,10 +61,10 @@ const operate = function() {
 
 const enterDigit = function() {
     if (!operator) {
-        num1 = this.textContent;
+        num1 += this.textContent;
         display.textContent += this.textContent;
     } else if (operator) {
-        num2 = this.textContent;
+        num2 += this.textContent;
         display.textContent += this.textContent;
     }
 }
@@ -63,6 +76,24 @@ const enterOperator = function() {
     }
 }
 
+const backSpace = function() {
+    display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+    if (num2) {
+        num2 = num2.slice(0, num2.length -1)
+    } else if (operator) {
+        operator = '';
+    } else if (num1) {
+        num1 = num1.slice(0, num1.length - 1)
+    }
+}
+
+const clearDisplay = function() {
+    display.textContent = '';
+    num1 = '';
+    num2 = '';
+    operator = '';
+}
+
 //Event listeners
 
 numBtn.forEach(element => element.addEventListener('click', enterDigit));
@@ -71,9 +102,6 @@ operatorBtn.forEach(element => element.addEventListener('click', enterOperator))
 
 equalsBtn.addEventListener('click', operate);
 
-clearBtn.addEventListener('click', () => {
-    display.textContent = '';
-    num1 = '';
-    num2 = '';
-    operator = '';
-});
+backSpaceBtn.addEventListener('click', backSpace);
+
+clearBtn.addEventListener('click', clearDisplay);
